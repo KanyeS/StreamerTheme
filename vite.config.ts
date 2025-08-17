0ee8e77bf560da224f2199686d681ed07b7209af
Kane <reillyj12345@gmail.com>
2025-08-17 21:11:07 +1000

    Fix Vite base path for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/streamertheme.vercel.app/",
+  base: "./",
   server: {
     port: 3000,
     host: '0.0.0.0'
