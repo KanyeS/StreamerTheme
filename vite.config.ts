b0a1f05b0d5bf748a23510615d3ada44c39b654d
Kane <reillyj12345@gmail.com>
2025-08-17 21:08:14 +1000

    Changed To Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/StreamerTheme",
+  base: "/streamertheme.vercel.app/",
   server: {
     port: 3000,
     host: '0.0.0.0'
